11da76a4f516cc720458d20b663e6462f003f18d
jhosuappp <joshua@150porciento.com>
2022-07-15 16:54:55 -0500

    Se adelanta primera parte del bot, primera pregunta + respuesta + validacion

--- public_html/theme/jhosuaTheme/template-part/chat-bot.php
+++ public_html/theme/jhosuaTheme/template-part/chat-bot.php
@@ -17,11 +17,14 @@
             <div class="bot-form__close">
             </div>
         </div>
-        <form class="bot-form">
+        <form class="bot-form" id="bot-form">
             <div class="bot-form__bloque">
-                <div class="bot-form__relative">
+
+                <!-- PRIMER BLOQUE DE PRESENTACION -->
+
+                <div class="bot-form__relative bot-form__relative--primero">
                     <div class="bot-form__mensaje bot-form__mensaje--animation">
-                        <p>!Hola¡</p>
+                        <p>¡Hola!</p>
                     </div>
                     <div class="bot-form__mensaje bot-form__mensaje--animation">
                         <p>Mi nombre es <b>Jhosua</b> y actualmente soy desarrollador front-end</p>
@@ -29,6 +32,22 @@
                     <div class="bot-form__mensaje bot-form__mensaje--animation">
                         <p>¿En qué estas interesado?</p>
                     </div>
+
+                    <!-- BLOQUE RADIO -->
+
+                    <div class="bot-form__mensaje bot-form__mensaje--animation bot-form__mensaje--radio">
+                        <input class="bot-form__radio__input" type="radio" name="interes" id="contratante" value="contrante">
+                        <label class="bot-form__radio__label" for="contratante">
+                            ¡Quiero contratarte!
+                        </label>
+                        <input class="bot-form__radio__input" type="radio" name="interes" id="cliente" value="cliente">
+                        <label  class="bot-form__radio__label" for="cliente">
+                            ¡Tengo un proyecto en mente!
+                        </label>
+                    </div>
+                    
+                    <!-- EFECTO TYPING -->
+
                     <div class="bot-form__mensaje bot-form__mensaje--typing">
                         <div class="bot-form__typing">
                             <div class="bot-form__typing--dot"></div>
@@ -37,16 +56,43 @@
                         </div>
                     </div>
                 </div>
-                <!-- <div class="bot-form__respuesta">
+
+                <!-- RESPUESTA -->
+
+                <div class="bot-form__respuesta bot-form__respuesta--radio">
                     <div class="bot-form__respuesta--bloque">
-                        <p>Lorem, ipsum dolor.</p>
+                        <p></p>
                     </div>
-                </div> -->
+                </div>
+
+                <!-- SEGUNDO BLOQUE DE NOMBRE -->
+
+                <div class="bot-form__relative bot-form__relative--segundo">
+                    <div class="bot-form__mensaje bot-form__mensaje--animation">
+                        <p></p>
+                    </div>
+                    <div class="bot-form__mensaje bot-form__mensaje--animation">
+                        <p>¿Cuál es tu nombre <b>completo</b></p>
+                    </div>
+                    
+                    <!-- EFECTO TYPING -->
+
+                    <div class="bot-form__mensaje bot-form__mensaje--typing">
+                        <div class="bot-form__typing">
+                            <div class="bot-form__typing--dot"></div>
+                            <div class="bot-form__typing--dot"></div>
+                            <div class="bot-form__typing--dot"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="bot-form__bloque--bottom">
-                <input type="text" placeholder="Escribe tu respuesta aquí" class="bot-form__input--respuesta">
+
+            <!-- BLOQUE DE RESPUESTA -->
+
+            <div class="bot-form__bloque--bottom bot-form__bloque--bottom--respuesta disable" id="bot-form__bloque-respuesta">
+                <input disable type="text" placeholder="Escribe tu respuesta aquí" id="respuesta" class="bot-form__input--respuesta">
                 <div class="bot-form__send--bloque">
-                    <button type="submit" class="bot-form__send">
+                    <button type="submit" class="bot-form__send" id="bot-form__send">
                         <svg width="25px" height="25px" viewBox="0 0 24 24" class="crt8y2ji"><path d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z"></path></svg>
                     </button>
                 </div>
--- sources/js/modules/Bot.js
+++ sources/js/modules/Bot.js
@@ -1,18 +1,23 @@
 var Bot = (function(){
 
-    const getBot = document.querySelector('.bot');
+    const getFormOfBot = document.querySelector('#bot-form');
+    const getInputRespuesta = document.querySelector('#respuesta');
+    const getButtonSend = document.querySelector('#bot-form__send');
+    const getBloqueRespuesta = document.querySelector('#bot-form__bloque-respuesta');
     const getFirtsContainerBot = document.querySelector('.bot__bloque--firts');
     const getSecondContainerBot = document.querySelector('.bot__bloque--second');
+
 
     function _bienvenidaBot(){
         var getMensajeBienvenida = document.querySelector('.bot__bloque--firts__left');
         var getBloqueRight = document.querySelector('.bot__bloque--firts__right');
 
+
         setTimeout(()=>{    
             getFirtsContainerBot.classList.add('active');
         },2000 );
 
-        //remove mensaje bienvenida
+        //QUITAR MENSAJE DE BIENVENIDA
         setTimeout(()=>{
             getMensajeBienvenida.classList.add('hidden');
             getBloqueRight.classList.add('active');
@@ -20,20 +25,16 @@ var Bot = (function(){
 
     }
 
-    function _scrollForm(){
-        var getBotFormBloque = document.querySelector('.bot-form__bloque');
-        var scrollHeightFormBloque = getBotFormBloque.scrollHeight;
-
-        getBotFormBloque .scroll(0, scrollHeightFormBloque);
-    }
+    //TODA FUNCIONALIDAD CUANDO SE ABRE EL BOT
 
     function _openBot(){
 
         function showMensajes(){
-            var getAllFormBloque = document.querySelectorAll('.bot-form__bloque .bot-form__mensaje--animation');
+            var getAllFormBloquePrimero = document.querySelectorAll('.bot-form__relative--primero .bot-form__mensaje--animation');
             var getMensajeForTyping = document.querySelector('.bot-form__mensaje--typing');
     
-            getAllFormBloque.forEach((data, indice)=>{
+
+            getAllFormBloquePrimero.forEach((data, indice)=>{
                 data.classList.add('active');
                 data.style.animation = `2s mensaje forwards 1 ${indice * 2.5 + "s"}`;
                 getMensajeForTyping.style.animation = `2s typingHidden forwards 1 ${indice * 2.5 + "s"}`;
@@ -45,13 +46,73 @@ var Bot = (function(){
             getSecondContainerBot.classList.add('active');
             showMensajes();
         });
+
     }
+
+
+    //TODA FUNCIONALIDAD CUANDO SE CIERRA EL BOT
 
     function _closeBot(){
         var getButtonCloseBot = document.querySelector('.bot-form__close');
 
+
         getButtonCloseBot.addEventListener('click', ()=>{
             getSecondContainerBot.classList.remove('active');
+        });
+
+    }
+
+    //FUNCIOANLIDAD DEL RADIO BUTTON, ADICIONAL VALIDACIÓN DEL SIGUIENTE MENSAJE
+
+    function _radioButton(){
+        var getAllRadioButtonsLabel = document.querySelectorAll('.bot-form__radio__label');
+        var getMensajeRadioButton = document.querySelector('.bot-form__mensaje--radio');
+        var getRespuestaRadioButton = document.querySelector('.bot-form__respuesta--radio');
+        var getRespuestaRadioButtonText = document.querySelector('.bot-form__respuesta--radio p')
+
+
+        getAllRadioButtonsLabel.forEach((data)=>{
+
+            data.addEventListener('click', ()=>{
+
+                getAllRadioButtonsLabel.forEach((data)=>{
+                    data.classList.remove('active');
+                });
+
+                data.classList.add('active');
+
+                getMensajeRadioButton.classList.add('hidden');
+                
+                //RESPUESTA
+                getRespuestaRadioButton.classList.add('active');
+                getRespuestaRadioButtonText.textContent = data.textContent;
+
+
+                //HABILITA CAMPO PARA ESCRIBIR RESPUESTA DEL NOMBRE
+
+                getBloqueRespuesta .classList.remove('disable');
+
+            });
+
+        });
+
+    }
+
+    //VALIDACION DE ENVIO Y VALIDACION DE DATOS
+
+    function _funcionalidadForm(){
+        var arrayWithDataForm = [];
+
+
+        //NO RECARGAR LA PAGINA
+        getFormOfBot.addEventListener('submit', (e)=>{
+            
+            var getValueInputRespuesta = getInputRespuesta.value;
+            
+            arrayWithDataForm.push(getValueInputRespuesta);
+
+            console.log(arrayWithDataForm);
+            e.preventDefault();
         });
     }
 
@@ -59,9 +120,10 @@ var Bot = (function(){
     return{
         setHandleEvent: function(){
             _bienvenidaBot()
-            _scrollForm()
             _openBot()
             _closeBot()
+            _radioButton()
+            _funcionalidadForm()
         }
     }
 
